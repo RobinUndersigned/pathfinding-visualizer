e6724c6af33ec46bd8d12dcefd32418e183c37ac
Robin <robin.decker@student.htw-berlin.de>
2021-04-16 13:31:09 +0200

    added grid styles and improvements

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,19 @@
 <template>
-  <main id="main-container" class="main-container">
     <header id="header-container" class="header-container">
       <div class="header-container-inner">
         Header
       </div>
     </header>
-    <section id="grid" class="grid-container">
-      <div v-for="row in grid" v-bind:key="row.id" class="grid-row">
-        <div v-for="node in row" v-bind:key="node.id" class="grid-node">
+    <section id="grid-container" class="grid-container">
+      <div id="grid" class="grid">
+        <div v-for="row in grid" v-bind:key="row.id" class="grid-row">
+          <div v-for="node in row" v-bind:key="node.id" v-bind:class="{ start: node.isStart, goal: node.isGoal }" class="grid-node">
 
+          </div>
         </div>
       </div>
     </section>
-  </main>
+
 </template>
 
 <script>
@@ -25,6 +26,8 @@ export default {
   },
   data() {
     return {
+      rowMax: 20,
+      colMax: 40,
       grid: []
     }
   },
@@ -32,10 +35,16 @@ export default {
 
   },
   mounted() {
-    for(let row = 0; row < 15; row++) {
+    for(let row = 0; row < this.rowMax; row++) {
       const currentRow = []
-      for(let col = 0; col < 40; col++) {
-        currentRow.push({x: col, y: row});
+      for(let col = 0; col < this.colMax; col++) {
+        let node = {
+          x: col,
+          y: row,
+          isStart: row == 3 && col == 5,
+          isGoal: row == 14 && col == 33
+        }
+        currentRow.push(node);
       }
       this.grid.push(currentRow);
     }
@@ -49,8 +58,9 @@ export default {
   --text-color-main: #1b272c;
   --font-family-main: Lato,Helvetica Neue,Helvetica,Roboto,Arial,sans-serif;
   --background-main: #ffffff;
-  --background-secondary: #f5f5f5;
+  --background-secondary: #001f3f;
   --node-size: 30px;
+  --grid-border-color: #111111;
 }
 
 * {
@@ -74,19 +84,18 @@ body {
 }
 
 #app {
-  min-height: 100vh;
-}
-
-.main-container {
   height: 100%;
   display: flex;
   flex-direction: column;
+  height: 100%;
 }
 
 .header-container {
-  height: 100px;
-  background: linear-gradient(360deg, var(--background-secondary), var(--background-main));
+  min-height: 100px;
+  display: block;
+  background: var(--background-secondary);
   border: 1px solid var(--background-secondary);
+  color: #80BFFFFF;
 }
 
 .header-container-inner {
@@ -94,20 +103,46 @@ body {
 }
 
 .grid-container {
-  flex: 1 1 0%;
-  display: flex;
-  flex-direction: column;
+  flex: 1 1 100%;
+  display: block;
+  height: 600px;
+}
+
+.grid {
+  height: 600px;
+  width: 1200px;
+  margin: 1rem auto;
 }
 
 .grid-row {
   width: 100%;
   display: flex;
   flex-direction: row;
+  height: 30px;
 }
+
+
 .grid-node {
   width: var(--node-size);
   height: var(--node-size);
-  border: 1px solid #1b272c;
+  border-top: 1px solid var(--grid-border-color);
+  border-left: 1px solid var(--grid-border-color);
+}
+
+.grid-node:last-of-type {
+  border-right: 1px solid var(--grid-border-color);
+}
+
+.grid-row:last-of-type > .grid-node {
+  border-bottom: 1px solid var(--grid-border-color);
+}
+
+.grid-node.start {
+  background-color: #01FF70;
+}
+
+.grid-node.goal {
+  background-color: #FF4136;
 }
 
 </style>
