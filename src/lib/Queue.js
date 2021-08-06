ef140bed241373afe068edd2ff246c0783ad0015
RobinUndersigned <r.decker@undersigned.de>
2021-08-06 15:49:19 +0200

    added Que Class

---
+++ src/lib/Queue.js
@@ -0,0 +1,28 @@
+export class Queue {
+  constructor() {
+    this.a = [];
+    this.b = 0;
+  }
+  getLength() {
+    return this.a.length - this.b;
+  };
+  isEmpty() {
+    return this.a.length == 0;
+  };
+  enqueue(b) {
+    this.a.push(b);
+  };
+  dequeue() {
+    if (0 != this.a.length) {
+      let c = this.a[this.b];
+      2 * ++this.b >= this.a.length && ((this.a = this.a.slice(this.b)), (this.b = 0));
+      return c;
+    }
+  };
+  peek() {
+    return 0 < this.a.length ? this.a[this.b] : void 0;
+  };
+  show(){
+    return this.a;
+  }
+}
